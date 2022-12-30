c2f5f46c7d09d624b1e3f0ad1272767abc0d8a48
Lucas Hammer <lucashammer10@yahoo.de>
2022-12-30 17:09:19 +0100

    random Coordinates

--- cognition-test-ui/src/data/constants.js
+++ cognition-test-ui/src/data/constants.js
@@ -1,7 +1,27 @@
-export const coordinates = [
-    {x: 10, y: 10},
-    {x: 40, y: 40},
-    {x: 70, y: 70},
-    {x: 100, y: 100},
-    {x: 130, y: 130}
-]
+export const coordinates = generateCoordinates()
+
+function generateCoordinates() {
+    let coordinates = []
+    let legalPoints = 0
+    while (legalPoints < 25) {
+        let point = generatePoint(20, 380)
+        if (collides(coordinates, point) === false){
+            coordinates.push(point)
+            legalPoints += 1
+        }
+    }
+    return coordinates
+};
+
+function generatePoint(min, max) {
+    return {x: Math.floor(Math.random() * (max - min)) + min, y: Math.floor(Math.random() * (max - min)) + min}
+  }
+
+function collides(coordinates, point){
+    for (const coord of coordinates){
+        if(Math.sqrt((coord.x - point.x)**2 + (coord.y - point.y)**2) < 30){
+            return true
+        }
+    }
+    return false
+}
